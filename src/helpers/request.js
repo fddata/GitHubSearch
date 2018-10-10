b7380f3b835f5a6a6f5e2c16b1deb99ee46ff8d7
fddata <fraserjdouglas@gmail.com>
2018-10-10 19:29:30 +0100

    adds index.html

---
+++ src/helpers/request.js
@@ -0,0 +1,12 @@
+const Request = function(url){
+  this.url = url;
+};
+
+
+Request.prototype.get = function () {
+  return fetch(this.url)
+    .then(response => response.json());
+};
+
+
+module.exports = Request;
